fixed bugs in data procesing and data normalisation, moced model files to better location

src/massdynamics/data_generation/data_processing.py
@@ -17,6 +17,20 @@ def normalise_data(strain, norm_factor = None):
     
     return np.array(strain)/norm_factor, norm_factor
 
+def unnormalise_data(strain, norm_factor = None):
+    """normalise the data to the maximum strain in all data
+
+    Args:
+        strain (_type_): strain array
+
+    Returns:
+        _type_: normalised strain
+    """
+    if norm_factor is None:
+        norm_factor = np.max(strain)
+    
+    return np.array(strain)*norm_factor, norm_factor
+
 def complex_to_real(input_array):
 
     output_array = np.concatenate(
@@ -108,10 +122,8 @@ def samples_to_positions_masses(
 
 def get_strain_from_samples(
     times, 
-    recon_masses, 
-    source_masses,
-    recon_coeffs, 
-    source_coeffs, 
+    masses, 
+    coeffs, 
     detectors=["H1"],
     return_windowed_coeffs=False, 
     window="none", 
@@ -134,45 +146,27 @@ def get_strain_from_samples(
     """
     # if there is a window and I and not predicting the windowed coefficients
    
-    n_masses, n_coeffs, n_dimensions = np.shape(recon_coeffs)
+    n_masses, n_coeffs, n_dimensions = np.shape(coeffs)
     if not return_windowed_coeffs and window != "none":
-        n_recon_coeffs = []
-        n_source_coeffs = []
+        n_coeffs = []
         # for each mass perform the window on the xyz positions (acceleration)
         for mass in range(n_masses):
-            temp_recon, win_coeffs = window_functions.perform_window(times, recon_coeffs[mass], window, basis_type=basis_type)
-            n_recon_coeffs.append(temp_recon)
-            if source_coeffs is not None:
-                temp_source, win_coeffs = window_functions.perform_window(times, source_coeffs[mass], window, basis_type=basis_type)
-                n_source_coeffs.append(temp_source)
-            
-
+            temp_recon, win_coeffs = window_functions.perform_window(times, coeffs[mass], window, basis_type=basis_type)
+            n_coeffs.append(temp_recon)
         
         # update the coefficients with the windowed version
-        recon_coeffs = np.array(n_recon_coeffs)
-        if source_coeffs is not None:
-            source_coeffs = np.array(n_source_coeffs)
+        coeffs = np.array(n_coeffs)
 
 
-    recon_strain, recon_energy = compute_waveform.get_waveform(
+    strain, energy = compute_waveform.get_waveform(
         times, 
-        recon_masses, 
-        recon_coeffs, 
+        masses, 
+        coeffs, 
         detectors, 
         basis_type=basis_type,
         compute_energy=True
     )
 
-    source_strain, source_energy = None, None
-    if source_coeffs is not None:
-        source_strain, source_energy = compute_waveform.get_waveform(
-            times, 
-            source_masses, 
-            source_coeffs, 
-            detectors, 
-            basis_type=basis_type,
-            compute_energy=True
-        )
 
-    return recon_strain, source_strain, recon_energy, source_energy, recon_coeffs, source_coeffs
+    return strain, energy, coeffs
 

src/massdynamics/data_generation/models/kepler_orbits.py
@@ -244,7 +244,7 @@ def generate_data(
     times = np.linspace(0,5e7, sample_rate)
     n_times = len(times)
 
-    semi_major_axes = np.random.uniform(0.1,1,size=n_samples)*distance_scale
+    semi_major_axes = np.random.uniform(0.3,1,size=n_samples)*distance_scale
     eccentricities = np.random.uniform(0.0,0.9,size=n_samples)
     inclinations = np.random.uniform(0.0,2*np.pi,size=n_samples)
 

src/massdynamics/data_generation/models/random_orbits.py
@@ -31,7 +31,7 @@ def generate_random_coefficients(
         halforder = int(order/2) + 1
         coefficients = np.zeros((halforder,n_dimensions), dtype=basis[basis_type]["dtype"])
         for i in range(halforder):
-            coefficients[i] = np.exp(-0.9*i) * (2*np.random.rand(n_dimensions)-1 + 1j*(2 * np.random.rand(n_dimensions) - 1))
+            coefficients[i] = np.exp(-fourier_weight*i) * (2*np.random.rand(n_dimensions)-1 + 1j*(2 * np.random.rand(n_dimensions) - 1))
 
     return coefficients
 
@@ -134,8 +134,14 @@ def generate_data(
                 basis_order, 
                 n_dimensions,
                 basis_type = basis_type,
-                fourier_weight=0.0)
-            all_basis_dynamics[data_index, mass_index] = coeffs.T 
+                fourier_weight=fourier_weight)
+
+            if window != "none":
+                coeffs = window_coeffs(times, random_coeffs, win_coeffs, basis_type=basis_type)
+            else:
+                coeffs = random_coeffs
+                
+            all_basis_dynamics[data_index, mass_index] = random_coeffs.T 
         
         
     """

src/massdynamics/test_model.py
@@ -40,11 +40,8 @@ def run_testing(config:dict) -> None:
         fourier_weight=config["fourier_weight"]
         )
 
-    try:
-        strain, norm_factor = data_processisng.normalise_data(strain, pre_model.norm_factor)
-    except:
-        print("WARNING: Normalising to different value")
-        strain, norm_factor = data_processing.normalise_data(strain, None)
+    strain, norm_factor = data_processing.normalise_data(strain, pre_model.norm_factor)
+
 
     """
     print(labels)
@@ -316,20 +313,38 @@ def test_model_3d(
             #ax.plot(recon_tseries[0][0], ls="--", color="r", label="remake")
             fig.savefig(os.path.join(root_dir, "test_pos2.png"))
             """
-            recon_strain, source_strain, recon_energy, source_energy, recon_coeffs, source_coeffs = data_processing.get_strain_from_samples(
+
+
+            recon_strain, recon_energy, recon_coeffs = data_processing.get_strain_from_samples(
                 times, 
                 recon_masses,
-                source_masses, 
                 recon_coeffs, 
+                detectors=detectors,
+                return_windowed_coeffs=return_windowed_coeffs, 
+                window=window, 
+                basis_type=basis_type)
+            """
+            source_strain, source_energy = compute_waveform.get_waveform(
+                times, 
+                source_masses, 
+                source_coeffs, 
+                detectors, 
+                basis_type=basis_type,
+                compute_energy=False)
+            """
+            
+            source_strain, source_energy,source_coeffs = data_processing.get_strain_from_samples(
+                times, 
+                source_masses,  
                 source_coeffs, 
                 detectors=detectors,
                 return_windowed_coeffs=return_windowed_coeffs, 
                 window=window, 
                 basis_type=basis_type)
-
+            
             recon_strain, _ = data_processing.normalise_data(recon_strain, pre_model.norm_factor)
             source_strain, _ = data_processing.normalise_data(source_strain, pre_model.norm_factor)
-
+            source_plot_data = data[0].cpu().numpy()
             #window = signal.windows.tukey(np.shape(source_strain)[-1], alpha=0.5)
             #recon_strain = recon_strain * window[None, :]
 
@@ -338,7 +353,7 @@ def test_model_3d(
                             detectors, 
                             recon_strain, 
                             source_strain, 
-                            data[0].cpu().numpy(), 
+                            source_plot_data, 
                             source_energy,
                             recon_energy,
                             fname = os.path.join(plot_out, f"reconstructed_{batch}.png"))
@@ -393,12 +408,10 @@ def test_model_3d(
                 m_recon_tseries[i] = t_time
                 m_recon_masses[i] = t_mass
 
-                temp_recon_strain, temp_recon_energy, _, _, temp_m_recon_coeffs, _ = data_processing.get_strain_from_samples(
+                temp_recon_strain, temp_recon_energy, temp_m_recon_coeffs = data_processing.get_strain_from_samples(
                     times, 
                     t_mass,
-                    None,
-                    t_co, 
-                    None, 
+                    t_co,  
                     detectors=["H1","L1","V1"],
                     return_windowed_coeffs=return_windowed_coeffs, 
                     window=window, 